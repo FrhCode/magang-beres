448d11cb02f205a4c8a83fa6c6c54501496029cf
FrhCode <farhan7534031b@gmail.com>
2023-07-08 10:31:10 +0700

    update

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ echo "Waiting for db"
 ./wait-for-it.sh database:3306 -t 100
 echo "db connedted"
 
-php artisan migrate:fresh --seed
+php artisan migrate
 
 php artisan serve --host=0.0.0.0 --port=8000
